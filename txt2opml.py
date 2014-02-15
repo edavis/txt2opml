0588d6cf8540883faedc8eb58835f84458ee27ea
Eric Davis <ed@npri.org>
2014-02-14 16:04:14 -0800

    txt2html.py: add <head> elements and write to a file

--- txt2opml.py
+++ txt2opml.py
@@ -6,9 +6,13 @@ txt2opml.py -- convert plain text outlines to OPML
 Adapted from: https://github.com/semk/Org2OPML/blob/master/org2opml.py
 """
 
+import os
 import re
+import time
+import datetime
 import argparse
 from lxml import etree
+
 
 class Node(object):
 
@@ -27,12 +31,30 @@ class Node(object):
         self.children.append(node)
 
 
-def build_opml(nodes):
+def add_header(head, key, value):
+    """
+    Set the header key to value.
+    """
+    etree.SubElement(head, key).text = value
+
+
+def stat_to_timestamp(s):
+    """
+    Convert UNIX epoch timestamp to RFC 822.
+    """
+    utc = time.gmtime(s)
+    d = datetime.datetime(*(utc[:6]))
+    return d.strftime('%a, %d %b %Y %H:%M:%S') + ' GMT'
+
+
+def build_opml(nodes, meta, output):
     """
     Build a complete OPML document.
     """
     opml_root = etree.Element('opml', version='2.0')
     opml_head = etree.SubElement(opml_root, 'head')
+    add_header(opml_head, 'title', meta['title'])
+    add_header(opml_head, 'dateModified', meta['dateModified'])
     opml_body = etree.SubElement(opml_root, 'body')
 
     def process_children(parent, node):
@@ -49,15 +71,26 @@ def build_opml(nodes):
         opml_summit = etree.SubElement(opml_body, 'outline', text=summit.text)
         process_children(opml_summit, summit)
 
-    print etree.tostring(opml_root, xml_declaration=True,
-                         pretty_print=True, encoding='UTF-8')
+    with open(output, 'wb') as fp:
+        content = etree.tostring(opml_root, xml_declaration=True,
+                                 pretty_print=True, encoding='UTF-8')
+        fp.write(content)
 
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--output')
     parser.add_argument('input')
     args = parser.parse_args()
 
+    if not args.output:
+        (base, ext) = os.path.splitext(args.input)
+        args.output = base + '.opml'
+
+    meta = {
+        'title': args.output,
+        'dateModified': stat_to_timestamp(os.stat(args.input).st_mtime),
+    }
     nodes = []
 
     with open(args.input) as fp:
@@ -97,7 +130,7 @@ def main():
             except IndexError:
                 nodes.append([node])
 
-    build_opml(nodes)
+    build_opml(nodes, meta, args.output)
 
 if __name__ == '__main__':
     main()
